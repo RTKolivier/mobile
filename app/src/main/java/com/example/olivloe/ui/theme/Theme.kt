84df8c375e68491f7a9e827e892f45eac133c02e
Dmitriy_Serov <dima.bessarab@list.ru>
2025-03-17 10:54:25 +0300

    1

---
+++ app/src/main/java/com/example/olivloe/ui/theme/Theme.kt
@@ -0,0 +1,103 @@
+package com.example.olivloe.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.olivloe.R
+import kotlinx.coroutines.selects.selectUnbiased
+
+@Immutable
+data class MatuleColors(
+    val block: Color,
+    val text: Color,
+    val subTextDark: Color,
+    val background: Color,
+    val hint: Color,
+)
+
+@Immutable
+data  class MatuleTextStyle(
+    val headingBold32: TextStyle,
+    val subTitleRegular16: TextStyle,
+    val bodyRegular16: TextStyle,
+    val bodyRegular14: TextStyle,
+    val bodyRegular12: TextStyle,
+)
+
+val localMatuleTypogtaphy = staticCompositionLocalOf {
+    MatuleTextStyle (
+        headingBold32 =  TextStyle.Default,
+        subTitleRegular16 =  TextStyle.Default,
+        bodyRegular12 = TextStyle.Default,
+        bodyRegular16 = TextStyle.Default,
+        bodyRegular14 = TextStyle.Default,
+
+    )
+}
+
+val localMatuleColors = staticCompositionLocalOf {
+    MatuleColors(
+        block = Color.Unspecified,
+        text = Color.Unspecified,
+        subTextDark = Color.Unspecified,
+        background = Color.Unspecified,
+        hint = Color.Unspecified
+    )
+}
+
+val matuleFontFamily = FontFamily(
+    Font(R.font.roboto_serif, FontWeight.Normal),
+    Font(R.font.roboto_serif_bold, FontWeight.Bold),
+    Font(R.font.roboto_serif_black, FontWeight.Black),
+    Font(R.font.roboto_serif_medium, FontWeight.Medium),
+    Font(R.font.roboto_serif_extrabold, FontWeight.ExtraBold),
+    Font(R.font.roboto_serif_semibold, FontWeight.SemiBold),
+)
+
+@Composable
+fun MatuleTheme(content: @Composable () -> Unit){
+    val matuleColors = MatuleColors(
+        block = Color(0xFFFFFFFF),
+        text = Color(0xFF2B2B2B),
+        subTextDark = Color(0xFF707B81),
+        background = Color(0xFF707881),
+        hint = Color(0xFF6A6A6A)
+    )
+    val matuleTypography = MatuleTextStyle(
+        headingBold32 = TextStyle(fontFamily = matuleFontFamily, fontWeight = FontWeight.Bold, fontSize = 32.sp),
+        subTitleRegular16 = TextStyle(fontFamily = matuleFontFamily, fontWeight = FontWeight.Normal, fontSize = 16.sp),
+        bodyRegular12 = TextStyle(fontFamily = matuleFontFamily, fontWeight = FontWeight.Normal, fontSize = 12.sp),
+        bodyRegular14 = TextStyle(fontFamily = matuleFontFamily, fontWeight = FontWeight.Normal, fontSize = 14.sp),
+        bodyRegular16 = TextStyle(fontFamily = matuleFontFamily, fontWeight = FontWeight.Normal, fontSize = 16.sp),
+    )
+    CompositionLocalProvider() {
+        localMatuleColors provides  matuleColors
+        localMatuleTypogtaphy provides matuleTypography
+    }
+    content()
+}
+
+object MatuleTheme {
+    val colors: MatuleColors
+    @Composable
+    get() = localMatuleColors.current
+val typography
+        @Composable
+        get() = localMatuleTypogtaphy.current
+}
---
+++ app/src/main/java/com/example/olivloe/ui/theme/screen/signIn/SignInScreen.kt
@@ -0,0 +1,5 @@
+package com.example.olivloe.ui.theme.screen.signIn
+
+import androidx.compose.runtime.Composable
+
+@Composable
